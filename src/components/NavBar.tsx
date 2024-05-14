90285718031bfe8bcbec89094a7f9dc1eb3d72df
Marat Salakh <marat.salakh.dev@gmail.com>
2024-05-14 11:06:53 +0200

    delete start code

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+  return (
+    <header className="flex justify-between">
+      <button>Gamification</button>
+      <div>
+        <button>Main</button>
+        <button>About</button>
+      </div>
+    </header>
+  );
+};
